12cd4a6c5d4ece8e192d3ae1fdddd8d7e2c4504a
Jonathan T. Dean <JonathanTomDean@Gmail.com>
2021-12-10 00:07:44 -0500

    Bitap is a little tricky to implement, switching to node sort

--- components/search/searchBar/main.tsx
+++ components/search/searchBar/main.tsx
@@ -44,6 +44,8 @@ function SearchBar() {
         e.preventDefault();
         setIsLoading(true);
 
+        console.log(`Bitap Results:`, bitapSearch(nameList[0], searchTerm));
+
         if(nameList.includes(searchTerm)) {
             setIsLoading(false);
             setSearchResults(nameList);
@@ -85,5 +87,63 @@ function SearchBar() {
         </div>
     )
 }
+
+// // This is a very simple implementation of the bitap algorithm.
+// const bitapSearch = (str: string, ptrn: string) => {
+//     // Define the max upper limit for pattern masking.
+//     const PATTERN_MASK_LIMIT = 128; 
+//     // Define the upper limit for the maximum allowable integer
+//     // for the pattern length implementation.
+//     const MAX_LENGTH = 32;
+//     // Due to pattern length, we can't go past a certain point
+//     // which is 32, or the value of a char. After 33 the pattern
+//     // doubles in time complexity.
+//     let ptrnLen: number = ptrn.length;
+//     // Data structure, specifically a Bit array. 
+//     // Stores the values to be checked against the pattern
+//     let R;
+
+//     // used to turn bits on and off in the bit array.
+//     let patternMask: number[] = new Array(PATTERN_MASK_LIMIT);
+
+//     // conditional check for null at ind 0
+//     if(ptrn[0] === '\0')  return 0;
+//     // Conditional for max length 32
+//     if(ptrnLen > 31) return -1;
+    
+//     // Initialize the value of <Bit[]> R
+//     // (~1)<<1 allows the pattern to be matched
+//     // against the input string. If we were to use
+//     // the following <R = ~ 1> then the pattern would
+//     // not be able to be matched against the input string.
+//     R = (~1) << 1;
+    
+//     // Begin the bit array with all bits on
+//     // and set the first bit to on.
+//     for(let i = 0; i <= PATTERN_MASK_LIMIT; ++i) {
+//         patternMask[i] = ~0; // set all bits to 1
+//     }
+
+//     // Iterate across the entire pattern and turn
+//     // off the bits that are not in the pattern.
+//     for(let i = 0; i < ptrnLen; ++i) {
+//         patternMask[ptrn[i]] &= ~(1 << i);
+//         console.log(`patternMask[${ptrn[i]}]`, patternMask[ptrn[i]]);
+//     }
+
+//     // Iterate through the string and check
+//     // the bits against the pattern. If 
+//     // the bit is null then the pattern is not found.
+//     for(let i = 0; str[i] !== '\0'; ++i) {
+//     	R |= patternMask[str[i]];
+// 		R <<= 1;
+
+//         console.log('Val of R: ', R)
+// 		if (0 == (R & (1 << ptrnLen))) return (i - ptrnLen) + 1;
+//     }
+
+//     console.log('failed to find pattern');
+//     return -1;
+// }
 
 export default SearchBar;
